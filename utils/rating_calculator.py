d7f5abb53dc9ec336bbaa2822c23548f5c263055
beerpiss <lacvtg.a1.2023@gmail.com>
2023-06-10 15:31:21 +0700

    rating_calculator: fix sss+ rating being floored to .14

--- utils/rating_calculator.py
+++ utils/rating_calculator.py
@@ -1,33 +1,28 @@
 from math import floor
 
-RATING_POINTS: list[dict[str, float]] = [
-    {"score": 1009000, "base": 21500, "ratio": 0},
-    {"score": 1007500, "base": 20000, "ratio": 1},
-    {"score": 1005000, "base": 15000, "ratio": 2},
-    {"score": 1000000, "base": 10000, "ratio": 1},
-    {"score": 975000, "base": 0, "ratio": 0.4},
-    {"score": 900000, "base": -50000, "ratio": 2 / 3},
-]
-
 
 def calculate_rating(score: int, internal_level: float) -> float:
-    _const = floor(internal_level * 10000)
+    level_base = internal_level * 100
 
-    if score >= 900000:
-        point: dict[str, float] = next(x for x in RATING_POINTS if score >= x["score"])
-        return (
-            max(0, _const + point["base"] + point["ratio"] * (score - point["score"]))
-            / 10000
-        )
+    rating100 = 0
 
-    rating = 0.0
-    if score >= 800000:
-        rating = (_const - 50000) / 2 + ((_const - 50000) / 2) * (
-            score - 800000
-        ) / 100000
-    elif score >= 500000:
-        rating = ((_const - 50000) / 2) * (score - 500000) / 300000
-    else:
-        rating = 0
-
-    return max(0, rating) / 10000
+    if score >= 1_009_000:
+        rating100 = level_base + 215
+    elif score >= 1_007_500:
+        rating100 = level_base + 200 + (score - 1_007_500) / 100
+    elif score >= 1_005_000:
+        rating100 = level_base + 150 + ((score - 1_005_000) * 10) / 500
+    elif score >= 1_000_000:
+        rating100 = level_base + 100 + ((score - 1_000_000) * 5) / 500
+    elif score >= 975_000:
+        rating100 = level_base + ((score - 975_000) * 2) / 500
+    elif score >= 925_000:
+        rating100 = level_base - 300 + ((score - 925_000) * 3) / 500
+    elif score >= 900_000:
+        rating100 = level_base - 500 + ((score - 900_000) * 4) / 500
+    elif score >= 800_000:
+        rating100 = (level_base - 500) / 2 + ((score - 800_000) * ((level_base - 500) / 2)) / 100_000
+    elif score >= 500_000:
+        rating100 = (((level_base - 500) / 2) * (score - 500_000)) / 300_000
+    
+    return max(floor(rating100) / 100, 0)
