6b8427615f24d222cd07aea943c41e7a853ca84f
beerpiss <lacvtg.a1.2023@gmail.com>
2023-06-16 11:24:46 +0700

    new way to log in

--- cogs/auth.py
+++ cogs/auth.py
@@ -1,3 +1,5 @@
+from asyncio import TimeoutError
+from secrets import SystemRandom
 from typing import Optional
 
 import discord
@@ -13,6 +15,7 @@ class AuthCog(commands.Cog, name="Auth"):
     def __init__(self, bot: ChuniBot) -> None:
         self.bot = bot
         self.utils: UtilsCog = self.bot.get_cog("Utils")  # type: ignore
+        self.random = SystemRandom()
 
     @commands.hybrid_command(
         name="logout",
@@ -24,6 +27,22 @@ class AuthCog(commands.Cog, name="Auth"):
         ):
             await self.bot.db.commit()
         await ctx.reply("Successfully logged out.", mention_author=False)
+
+    async def _verify_and_login(self, id: int, clal: str) -> Optional[Exception]:
+        if clal.startswith("clal="):
+            clal = clal[5:]
+
+        async with ChuniNet(clal) as client:
+            try:
+                await client.validate_cookie()
+                async with self.bot.db.execute(
+                    "INSERT INTO cookies VALUES (?, ?) ON CONFLICT(discord_id) DO UPDATE SET cookie=excluded.cookie",
+                    (id, clal),
+                ):
+                    await self.bot.db.commit()
+                return
+            except Exception as e:
+                return e
 
     @commands.hybrid_command("login")
     async def login(self, ctx: Context, clal: Optional[str] = None):
@@ -55,45 +74,62 @@ class AuthCog(commands.Cog, name="Auth"):
                 f"Login instructions have been sent to your DMs. {please_delete_message}"
                 "(please **enable Privacy Settings -> Direct Messages** if you haven't received it.)"
             )
-        if clal is None:
-            embed = discord.Embed(
-                title="How to login",
-                description=(
-                    "**Step 1**\n"
-                    "Log in to your account on [CHUNITHM-NET](https://chunthm-net-eng.com).\n"
-                    "**Please do so in an incognito window!** Not doing so may cause the account to be logged out unexpectedly.\n"
-                    "\n"
-                    "Then enter [this webpage](https://lng-tgk-aime-gw.am-all.net/common_auth/). You should see a `Not Found` error.\n"
-                    "Open developer tools (Ctrl + Shift + I or F12) and paste this into the console:\n"
-                    "```js\n"
-                    "(function(d){c=(n)=>Object.fromEntries(d.cookie.split(';').map(c=>c.split('=')))[n];confirm(`Paste this in the bot's DMs:\\nc>login ${c('clal')}\\nPress OK to copy.`)&&navigator.clipboard.writeText(`c>login ${c('clal')}`)})(document)\n"
-                    "```\n"
-                    "(This cookie cannot access your Aime account! It can only be used to login to CHUNITHM-NET.)\n\n"
-                    "**Step 2**\n"
-                    "Send `c>login <cookie>` without the angled brackets.\n"
-                    "\n"
-                    "If the cookie is undefined, or if there is extra data after `;`, please logout then login and try again.\n"
-                ),
-            )
-            if ctx.channel == channel:
-                return await ctx.reply(embed=embed, mention_author=False)
+
+        if clal is not None:
+            if (e := await self._verify_and_login(ctx.author.id, clal)) is None:
+                return await channel.send("Successfully logged in.")
             else:
-                return await channel.send(embed=embed)
-
-        if clal.startswith("clal="):
-            clal = clal[5:]
-
-        async with ChuniNet(clal) as client:
-            try:
-                await client.validate_cookie()
-                async with self.bot.db.execute(
-                    "INSERT INTO cookies VALUES (?, ?) ON CONFLICT(discord_id) DO UPDATE SET cookie=excluded.cookie",
-                    (ctx.author.id, clal),
-                ):
-                    await self.bot.db.commit()
-                await ctx.send("Successfully logged in.")
-            except Exception as e:
                 raise commands.BadArgument(f"Invalid cookie: {e}")
+
+        passcode = str(self.random.randrange(1000, 10000))
+        embed = discord.Embed(
+            title="How to login",
+            description=(
+                "Log in to your account on [CHUNITHM-NET](https://chunthm-net-eng.com).\n"
+                "**Please do so in an incognito window!** Not doing so may cause the account to be logged out unexpectedly.\n"
+                "\n"
+                f"Then enter [this webpage](https://lng-tgk-aime-gw.am-all.net/common_auth/?otp={passcode}). You should see a `Not Found` error.\n"
+                "Open developer tools (Ctrl + Shift + I or F12) and paste this into the console:\n"
+                "```js\n"
+                "(function(d){c=(n)=>Object.fromEntries(d.cookie.split(';').map(c=>c.split('=')))[n];confirm(`Paste this in the bot's DMs:\\nc>login ${c('clal')}\\nPress OK to copy.`)&&navigator.clipboard.writeText(`c>login ${c('clal')}`)})(document)\n"
+                "```\n"
+                "(This cookie cannot access your Aime account! It can only be used to login to CHUNITHM-NET.)\n"
+                f"In case the website asks for a passcode, type {passcode} and select OK.\n"
+            ),
+        )
+        if ctx.channel == channel:
+            msg = await ctx.reply(embed=embed, mention_author=False)
+        else:
+            msg = await channel.send(embed=embed)
+
+        def check(otp: str, _: str):
+            return otp == passcode
+
+        try:
+            _, clal = await self.bot.wait_for(
+                "chunithm_login", check=check, timeout=120
+            )
+            if (e := await self._verify_and_login(ctx.author.id, clal)) is None:  # type: ignore
+                await msg.edit(
+                    embed=discord.Embed(
+                        title="Successfully logged in",
+                        description="You can now use the bot's CHUNITHM-NET commands.",
+                    )
+                )
+            else:
+                await msg.edit(
+                    embed=discord.Embed(
+                        title="Failed to login",
+                        description=f"Invalid cookie: {e}",
+                    )
+                )
+        except TimeoutError:
+            await msg.edit(
+                embed=discord.Embed(
+                    title="Login session timed out",
+                    description="Please use `c>login` to restart the login process.",
+                )
+            )
 
 
 async def setup(bot: ChuniBot) -> None:
---
+++ web.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING
+
+from aiohttp import web
+
+if TYPE_CHECKING:
+    from bot import ChuniBot
+
+
+router = web.RouteTableDef()
+
+
+@router.post("/chuninewbot/login")
+async def login(request: web.Request) -> web.Response:
+    params = {}
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        params = await request.json()
+    elif content_type in ["application/x-www-form-urlencoded", "multipart/form-data"]:
+        params = await request.post()
+    else:
+        raise web.HTTPBadRequest(reason="Invalid Content-Type")
+
+    if "otp" not in params or "clal" not in params:
+        raise web.HTTPBadRequest(reason="Missing parameters")
+
+    request.config_dict["bot"].dispatch("chunithm_login", params["otp"], params["clal"])
+    return web.Response(
+        text="""<h1>Success!</h1>
+<h5>Check the bot's DMs to see if the account has been successfully linked.</h5>
+
+<img src="https://chunithm-net-eng.com/mobile/images/pen_sleep_apng.png">
+"""
+    )
+
+
+def init_app(bot: "ChuniBot") -> web.Application:
+    app = web.Application()
+    app.add_routes(router)
+    app["bot"] = bot
+    return app
