aa0266313cfd1bef17572bf7f2df1d88b83cd17c
tukkun1995 <139703070+tukkun1995@users.noreply.github.com>
2023-08-16 01:15:16 +0700

    Add c>border
    Add command c>border <difficulty> <query>

--- utils/components/chart_card_embed.py
+++ utils/components/chart_card_embed.py
@@ -4,16 +4,17 @@ import discord
 from discord.utils import escape_markdown
 
 from chunithm_net.consts import JACKET_BASE
-from chunithm_net.entities.enums import Difficulty
-from utils import sdvxin_link, yt_search_link
+from chunithm_net.entities.enums import Difficulty, Rank
+from utils import floor_to_ndp, sdvxin_link, yt_search_link
 from utils.calculation.rating import calculate_rating
+from utils.ranks import rank_icon
 
 if TYPE_CHECKING:
     from database.models import Chart
 
 
 class ChartCardEmbed(discord.Embed):
-    def __init__(self, chart: "Chart", *, target_score: Optional[int] = None) -> None:
+    def __init__(self, chart: "Chart", *, target_score: Optional[int] = None, border = False) -> None:
         difficulty = Difficulty.from_short_form(chart.difficulty)
 
         super().__init__(
@@ -52,3 +53,53 @@ class ChartCardEmbed(discord.Embed):
                 name="Target Score",
                 value=field_value,
             )
+
+        if border:
+            if chart.maxcombo is not None:
+                field_value = str(chart.maxcombo)
+                
+                tolerance_sssp = floor_to_ndp(chart.maxcombo / 10, 0)
+                tolerance_sss = floor_to_ndp(chart.maxcombo / 4, 0)
+                tolerance_ssp = floor_to_ndp(chart.maxcombo / 2, 0)
+                tolerance_ss = floor_to_ndp(chart.maxcombo, 0)
+                tolerance_sp = floor_to_ndp(chart.maxcombo * 2, 0)
+                tolerance_s = floor_to_ndp(chart.maxcombo * 3.5, 0)
+
+                border_miss_sssp = 0
+                border_miss_sss = 0
+                border_miss_ssp = floor_to_ndp(tolerance_ssp / 300, 0)
+                border_miss_ss = floor_to_ndp(tolerance_ss / 275, 0)
+                border_miss_sp = floor_to_ndp(tolerance_sp / 250, 0)
+                border_miss_s = floor_to_ndp(tolerance_s / 200, 0)
+
+                border_atk_sssp = floor_to_ndp(tolerance_sssp / 60, 0) - border_miss_sssp * 2
+                border_atk_sss = floor_to_ndp(tolerance_sss / 59, 0) - border_miss_sss * 2
+                border_atk_ssp = floor_to_ndp(tolerance_ssp / 58, 0) - border_miss_ssp * 2
+                border_atk_ss = floor_to_ndp(tolerance_ss / 56, 0) - border_miss_ss * 2
+                border_atk_sp = floor_to_ndp(tolerance_sp / 54, 0) - border_miss_sp * 2
+                border_atk_s = floor_to_ndp(tolerance_s / 53, 0) - border_miss_s * 2
+
+                border_jus_sssp = tolerance_sssp - border_atk_sssp * 51 - border_miss_sssp * 101
+                border_jus_sss = tolerance_sss - border_atk_sss * 51 - border_miss_sss * 101
+                border_jus_ssp = tolerance_ssp - border_atk_ssp * 51 - border_miss_ssp * 101
+                border_jus_ss = tolerance_ss - border_atk_ss * 51 - border_miss_ss * 101
+                border_jus_sp = tolerance_sp - border_atk_sp * 51 - border_miss_sp * 101
+                border_jus_s = tolerance_s - border_atk_s * 51 - border_miss_s * 101
+
+                self.add_field(
+                    name="Note Count",
+                    value=field_value,
+                )
+
+                self.add_field(
+                    name="Borders (JUSTICE-ATTACK-MISS)",
+                    value=(
+                        f"▸ {rank_icon(Rank.SSSp)} ▸ {border_jus_sssp:.0f}-{border_atk_sssp:.0f}-{border_miss_sssp:.0f}\n"
+                        f"▸ {rank_icon(Rank.SSS)} ▸ {border_jus_sss:.0f}-{border_atk_sss:.0f}-{border_miss_sss:.0f}\n"
+                        f"▸ {rank_icon(Rank.SSp)} ▸ {border_jus_ssp:.0f}-{border_atk_ssp:.0f}-{border_miss_ssp:.0f}\n"
+                        f"▸ {rank_icon(Rank.SS)} ▸ {border_jus_ss:.0f}-{border_atk_ss:.0f}-{border_miss_ss:.0f}\n"
+                        f"▸ {rank_icon(Rank.Sp)} ▸ {border_jus_sp:.0f}-{border_atk_sp:.0f}-{border_miss_sp:.0f}\n"
+                        f"▸ {rank_icon(Rank.S)} ▸ {border_jus_s:.0f}-{border_atk_s:.0f}-{border_miss_s:.0f}"
+                    ),
+                )
+
