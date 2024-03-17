ba73711627c8984e0cee60342fa27e1ff1d0435d
beerpiss <lacvtg.a1.2023@gmail.com>
2024-03-17 19:43:41 +0700

    dbutils(sdvxin): Add alias for Blow My Mind

--- dbutils.py
+++ dbutils.py
@@ -274,6 +274,7 @@ async def update_sdvxin(async_session: async_sessionmaker[AsyncSession]):
         "AstroNotes.": "AstrøNotes.",
         "Athlete Killer ”Meteor”": 'Athlete Killer "Meteor"',
         "Aventyr": "Äventyr",
+        "Blow my mind": "Blow My Mind",
         "Chaotic Order": "Chaotic Ørder",
         "DAZZLING SEASON": "DAZZLING♡SEASON",
         "DON`T STOP ROCKIN` ~[O_O] MIX~": "D✪N`T ST✪P R✪CKIN` ~[✪_✪] MIX~",
