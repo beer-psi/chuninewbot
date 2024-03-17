e8e087bad2091ba472532b4a1903b70c66a0d0c6
beerpiss <lacvtg.a1.2023@gmail.com>
2024-03-18 00:41:06 +0700

    minor spelling mistake.gif

--- utils/constants.py
+++ utils/constants.py
@@ -4,5 +4,5 @@ SIMILARITY_THRESHOLD = 65
 # The version names are just my favorite CHUNITHM songs.
 VERSION_NAMES = {
     "v0.2.1": "Ray of Hope",
-    "v0.2.2": "pavorbital",
+    "v0.2.2": "parvorbital",
 }
