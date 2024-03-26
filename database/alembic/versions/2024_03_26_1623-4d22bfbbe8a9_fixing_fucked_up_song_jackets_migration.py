708105dfc39d680d15e1703b92b1de7213823a70
beerpiss <lacvtg.a1.2023@gmail.com>
2024-03-26 16:25:51 +0700

    db: fix incorrect song_jackets foreign key

---
+++ database/alembic/versions/2024_03_26_1623-4d22bfbbe8a9_fixing_fucked_up_song_jackets_migration.py
@@ -0,0 +1,43 @@
+"""Fixing fucked up song_jackets migration
+
+Revision ID: 4d22bfbbe8a9
+Revises: f38b7c3d0930
+Create Date: 2024-03-26 16:23:18.667950
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "4d22bfbbe8a9"
+down_revision: Union[str, None] = "f38b7c3d0930"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with op.batch_alter_table("song_jackets") as bop:
+        bop.drop_constraint("fk_chunirec_songs_id")
+        bop.create_foreign_key(
+            "fk_chunirec_songs_id",
+            "chunirec_songs",
+            ["song_id"],
+            ["id"],
+            ondelete="cascade",
+            onupdate="cascade",
+        )
+
+
+def downgrade() -> None:
+    with op.batch_alter_table("song_jackets") as bop:
+        bop.drop_constraint("fk_chunirec_songs_id")
+        bop.create_foreign_key(
+            "fk_chunirec_songs_id",
+            "chunirec_songs",
+            ["song_id"],
+            ["song"],
+            ondelete="cascade",
+            onupdate="cascade",
+        )
