fea21e721d2eda52979fbff3a4b83f1f66c0a55c
beerpiss <lacvtg.a1.2023@gmail.com>
2024-05-16 08:02:39 +0700

    chore: sdvxin alias

--- dbutils/sdvxin.py
+++ dbutils/sdvxin.py
@@ -44,6 +44,7 @@ TITLE_MAPPING = {
     "Chaotic Order": "Chaotic Ørder",
     "DAZZLING SEASON": "DAZZLING♡SEASON",
     "DON`T STOP ROCKIN` ~[O_O] MIX~": "D✪N`T ST✪P R✪CKIN` ~[✪_✪] MIX~",
+    "DON’T STOP ROCKIN’ ～[O_O] MIX": "D✪N’T ST✪P R✪CKIN’ ～[✪_✪] MIX～",
     "DON’T STOP ROCKIN’ ～[O_O] MIX～": "D✪N’T ST✪P R✪CKIN’ ～[✪_✪] MIX～",
     "Daydream cafe": "Daydream café",
     "Defandour": "Dèfandour",
